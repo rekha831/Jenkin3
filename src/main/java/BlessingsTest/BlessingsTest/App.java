a476e3dcb68412aa737516f9ed24440f1fb6b19e
Kumar <adarsh.s.kumar@accenture.com>
2021-08-19 20:27:28 +0530

    this is my first commit

---
+++ src/main/java/BlessingsTest/BlessingsTest/App.java
@@ -0,0 +1,13 @@
+package BlessingsTest.BlessingsTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
